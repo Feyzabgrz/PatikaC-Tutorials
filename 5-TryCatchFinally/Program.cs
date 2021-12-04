fd35c02076b62c75acdda2eb887a251e268b6943
Feyza Bağıröz <Feyzabgrz@gmail.com>
2021-12-05 01:08:24 +0300

    TryCatchFinally

---
+++ 5-TryCatchFinally/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace PatikaCSharpTutorials
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            try{
+                Console.WriteLine("Bir Sayı giriniz:");
+                int sayi=Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayı:" +sayi);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hata:" + ex.Message.ToString());
+            }
+            // finally
+            // {
+            //    //Kod bloğu hata alsa bile çalışmasını istediğimiz durumları yazıyoruz
+            //     Console.WriteLine("İşlem Tamamlandı.");
+            // }
+
+
+            //Özel Exceptipn mesajları
+
+
+              try
+              {
+                  //int a=int.Parse(null); 
+                  //int a=int.Parse("test");
+                  int a=int.Parse("-20000000");
+              }
+            //   catch (ArgumentNullException ex)
+            //   {
+            //     Console.WriteLine("Boş değer girdiniz");
+            //     Console.WriteLine(ex);
+                  
+            //   }      
+              catch (FormatException ex)
+              {
+                Console.WriteLine("Veri tipi uygun değil");
+                Console.WriteLine(ex);
+                  
+              }      
+              catch (OverflowException ex)
+              {
+                Console.WriteLine("Çok küçük ya da çok büyük bir değer girdiniz!");
+                Console.WriteLine(ex);
+                  
+              }      
+
+        }
+    }
+}
